92f0e5004a9ba603d8ca907309679aa0f4c77b1c
David Feimer <davidfeimer@gmail.com>
2015-04-03 22:23:49 +0000

    Bonus changes to code

--- math_game.rb
+++ math_game.rb
@@ -1,29 +1,50 @@
+require 'colorize'
 
-# initialize player_lives = 3
 @player1 = {name: "Player1",
-  lives: 3}
+  lives: 3, score: 0}
 @player2 = {name: "Player2",
-  lives: 3}     
+  lives: 3, score: 0}     
 @current_player = @player1   
+
+def get_name
+  if @player1[:name] == "Player1"
+  puts "Player 1 What is your name?"
+  @player1[:name] = gets.chomp.green
+end
+  if @player2[:name] == "Player2"
+  puts "Player 2 What is your name?"
+  @player2[:name] = gets.chomp.yellow
+end
+end
+
+def randomizer
+  random = rand(1..3)
+  case random
+  when 1
+    @answer = @first_number + @second_number
+    puts "#{@current_player[:name]} : " + "What is #{@first_number} + #{@second_number}?".colorize(:light_blue)
+  when 2
+    @answer = @first_number * @second_number
+    puts "#{@current_player[:name]} : " + "What is #{@first_number} x #{@second_number}?".colorize(:light_blue)
+  when 3
+    @answer = @first_number - @second_number
+    puts "#{@current_player[:name]} : " + " What is #{@first_number} - #{@second_number}?".colorize(:light_blue)
+  end
+end
 
 def generate_question
   @first_number = rand(1...20)
   @second_number = rand(1...20)
-  @answer = @first_number + @second_number
-  puts "#{@current_player[:name]} What is #{@first_number} + #{@second_number}?"
+  randomizer
 end
-# define generate_question
 
-def run
-  until (@player1[:lives] == 0 || @player2[:lives] == 0) do
-     generate_question
-     player_answer = gets.chomp
-     if player_answer.to_i!= @answer
-      @current_player[:lives] -= 1
-      puts "#{@current_player[:name]} Lost One Life"
-      puts "Lives: #{@player1[:name]} = #{@player1[:lives]} #{@player2[:name]} = #{@player2[:lives]}"
-    end
-    switch_player
+def winner
+  if @player1[:lives] > @player2[:lives]
+    puts "#{@player1[:name]} is the Winner!"
+    puts "With a Score of #{@player1[:score]}!"
+  else
+    puts "#{@player2[:name]} is the Winner!"
+    puts "With a Score of #{@player1[:score]}!"
   end
 end
 
@@ -35,18 +56,45 @@ def switch_player
   end
 end
 
-run
-# loop until player1_lives || player2_lives = 0
-#   generate_question for player1
-#   if player1_answer = generate_question
-#     generate_question for player2
+def run
+  get_name
+  until (@player1[:lives] == 0 || @player2[:lives] == 0) do
+     generate_question
+     player_answer = gets.chomp
+     if 
+      player_answer.to_i != @answer
+      @current_player[:lives] -= 1
+      puts
+      puts "#{@current_player[:name]} Lost One Life"
+      puts
+      puts "Lives: #{@player1[:name]} = #{@player1[:lives]} #{@player2[:name]} = #{@player2[:lives]}" 
+      puts
+    else
+      @current_player[:score] += 1
+      puts
+      puts "Correct!".colorize(:cyan)
+      puts
+    end
+    switch_player
+  end
+  winner
+end
 
-#   else player1_answer != generate_question
-#     player1_lives -= 1 
-#     return player_lives
-#   end
-# if player1_lives || player2_lives = 0
-    
-#   end
+def again
+  puts "Would you like to play again? (yes/no)"
+  play_again = gets.chomp.downcase
+  if play_again == 'yes'
+    @player1[:lives] = 3
+    @player2[:lives] = 3
+    run
+  else
+    puts "Good-Bye"
+  end
+end
+
+run
+again
+
+
 
 
